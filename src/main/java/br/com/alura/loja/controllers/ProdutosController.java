463825c0c6ea4e9bedceb12eeea6c3730aa94bc5
Marcelo <marcelo.home19@gmail.com>
2019-11-12 12:32:24 -0200

    Preparando o produtosController

--- src/main/java/br/com/alura/loja/controllers/ProdutosController.java
+++ src/main/java/br/com/alura/loja/controllers/ProdutosController.java
@@ -3,11 +3,18 @@ package br.com.alura.loja.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import br.com.alura.loja.models.Produto;
+
 @Controller
 public class ProdutosController {	
 	
 	@RequestMapping("/produtos/form")
 	public String form() {
 		return "produtos/form";
+	}
+	
+	@RequestMapping("/produtos")
+	public String grava(Produto produto) {
+		return "produtos/ok";
 	}
 }
--- src/main/webapp/WEB-INF/views/produtos/form.jsp
+++ src/main/webapp/WEB-INF/views/produtos/form.jsp
@@ -8,7 +8,7 @@
 </head>
 <body>
 
-	<form action = "/produtos" method="post">
+	<form action = "/casadocodigo/produtos" method="post">
 		<div>
 			<label>Título</label>
 			<input type = "text" name = "titulo">
