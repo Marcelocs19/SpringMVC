5edde32c3f59015e8957228b54533ed4b06bc6c8
Marcelo <marcelo.home19@gmail.com>
2019-11-13 13:21:06 -0200

    Modelando o tipo e preço dos produtos

--- src/main/java/br/com/alura/loja/models/Produto.java
+++ src/main/java/br/com/alura/loja/models/Produto.java
@@ -1,5 +1,8 @@
 package br.com.alura.loja.models;
 
+import java.util.List;
+
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -17,7 +20,18 @@ public class Produto {
 	private String descricao;
 	
 	private int paginas;
+	
+	@ElementCollection
+	private List<Preco> precos;
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
 	public String getTitulo() {
 		return titulo;
 	}
@@ -40,6 +54,14 @@ public class Produto {
 
 	public void setPaginas(int paginas) {
 		this.paginas = paginas;
+	}
+
+	public List<Preco> getPrecos() {
+		return precos;
+	}
+
+	public void setPrecos(List<Preco> precos) {
+		this.precos = precos;
 	}
 
 	@Override
