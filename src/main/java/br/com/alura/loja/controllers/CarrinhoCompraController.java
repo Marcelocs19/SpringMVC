3fd44e1cce280214a6f85203c68cb636fc9a4acb
Marcelo <marcelo.home19@gmail.com>
2019-11-19 13:41:37 -0200

    Expondo atributos no jsp

---
+++ src/main/java/br/com/alura/loja/controllers/CarrinhoCompraController.java
@@ -0,0 +1,38 @@
+package br.com.alura.loja.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.alura.loja.daos.ProdutoDAO;
+import br.com.alura.loja.models.CarrinhoCompras;
+import br.com.alura.loja.models.CarrinhoItem;
+import br.com.alura.loja.models.Produto;
+import br.com.alura.loja.models.TipoPreco;
+
+@Controller
+@RequestMapping("/carrinho")
+public class CarrinhoCompraController {
+	
+	@Autowired
+	private ProdutoDAO produtoDAO;
+	
+	@Autowired
+	private CarrinhoCompras carrinho;
+	
+	@RequestMapping("/add")
+	public ModelAndView add(Integer produtoId, TipoPreco tipoPreco) {
+		ModelAndView modelAndView = new ModelAndView("redirect:/produtos");
+		CarrinhoItem carrinhoItem = criaItem(produtoId, tipoPreco);
+		carrinho.add(carrinhoItem);
+		return modelAndView;
+	}
+	
+	private CarrinhoItem criaItem(Integer produtoId, TipoPreco tipoPreco) {
+		Produto produto = produtoDAO.find(produtoId);
+		CarrinhoItem carrinhoItem = new CarrinhoItem(produto, tipoPreco);
+		return carrinhoItem;
+		
+	}
+}
