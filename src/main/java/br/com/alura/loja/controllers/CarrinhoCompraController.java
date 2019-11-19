0202760786f95afa7689e1682d59e414a3bb6eb2
Marcelo <marcelo.home19@gmail.com>
2019-11-19 14:27:02 -0200

    preparando projeto para listar os itens da compra

--- src/main/java/br/com/alura/loja/controllers/CarrinhoCompraController.java
+++ src/main/java/br/com/alura/loja/controllers/CarrinhoCompraController.java
@@ -1,8 +1,10 @@
 package br.com.alura.loja.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.alura.loja.daos.ProdutoDAO;
@@ -13,6 +15,7 @@ import br.com.alura.loja.models.TipoPreco;
 
 @Controller
 @RequestMapping("/carrinho")
+@Scope(value=WebApplicationContext.SCOPE_REQUEST)
 public class CarrinhoCompraController {
 	
 	@Autowired
